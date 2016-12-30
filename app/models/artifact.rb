4164484063f98f1ed15e4382ae189af5ec9c45f7
Rick Thornbrugh <rthornbrug@gmail.com>
2016-12-30 22:21:45 +0000

    aws s3 bucket is working correctly.

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -13,9 +13,9 @@ class Artifact < ActiveRecord::Base
   private
   
   def upload_to_s3
-    s3 = AWS::S3::Resource.new
+    s3 = Aws::S3::Resource.new
     tenant_name = Tenant.find(Thread.current[:tenant_id]).name
-    obj = s3.bucket(ENV['AWS_S3_BUCKET']).object("#{tenant_name}/#{upload.original_filename}")
+    obj = s3.bucket(ENV['S3_BUCKET']).object("#{tenant_name}/#{upload.original_filename}")
     obj.upload_file(upload.path, acl:'public-read')
     self.key = obj.public_url
   end  
