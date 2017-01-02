60fae26d09f071946ed780fd341205a6a19274a6
Rick Thornbrugh <rthornbrug@gmail.com>
2017-01-02 23:43:42 +0000

    attempting db fix.

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :address => 'smpt.sendgrid.net',
+  :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
   :user_name => ENV['SENDGRID_USERNAME'],
