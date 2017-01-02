47fd3463342eb550b89a4829faeea76430e9e8cd
Rick Thornbrugh <rthornbrug@gmail.com>
2017-01-02 23:07:26 +0000

    attempting db fix.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'mind-effects-matter-thornbrugh.herokuapp.com', :protocol => 'https' }
+  config.action_mailer.default_url_options = { :host => 'mind-effects-matter-rthornbrug.herokuapp.com', :protocol => 'https' }
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
